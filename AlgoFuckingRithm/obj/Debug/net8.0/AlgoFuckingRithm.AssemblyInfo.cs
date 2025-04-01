2a3db454c482586e7ca3636621fb0c93494cb7ca
IchiHiramami <159204849+IchiHiramami@users.noreply.github.com>
2025-04-01 20:56:59 +0800

    Changes

--- AlgoFuckingRithm/obj/Debug/net8.0/AlgoFuckingRithm.AssemblyInfo.cs
+++ AlgoFuckingRithm/obj/Debug/net8.0/AlgoFuckingRithm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgoFuckingRithm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57acab49a838df9d442ca20743a8f4ecabb0881a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgoFuckingRithm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgoFuckingRithm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
